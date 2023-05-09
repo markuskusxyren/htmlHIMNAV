c48ff5989e3646cf283ce6456557f1f31b145341
markuskusxyren <mxlfranco2020@plm.edu.ph>
2023-05-09 15:24:21 +0800

    changed alot of js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 mapboxgl.accessToken =
   'pk.eyJ1Ijoia3Vza3VzeHlyZW5uIiwiYSI6ImNsZWN4ampubzAxaDczcG16MXcwcWhhcDEifQ.9K3JBDAzq3Ru8riWg49zgw';
 
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-  enableHighAccuracy: true,
-});
-
-function successLocation(position) {
-  setupMap([position.coords.longitude, position.coords.latitude]);
-}
-
-function errorLocation() {
-  setupMap([121.0524150628587, 14.682569991056297]);
-}
-
 function getUnitCoordinates(unitId, map) {
   const unitRef = db.collection('tombs').doc(unitId);
 
@@ -72,33 +60,39 @@ function loadUnits(map) {
 }
 
 function setupMap(center = [121.0524150628587, 14.682569991056297]) {
-  map = new mapboxgl.Map({
+  const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/kuskusxyrenn/clee7imbg000p01nx6ah0pt8w',
+    center: center,
     zoom: 15,
   });
 
   const nav = new mapboxgl.NavigationControl();
-
   map.addControl(nav);
 
   var directions = new MapboxDirections({
     accessToken: mapboxgl.accessToken,
   });
-
   map.addControl(directions, 'top-left');
 
-  map.addControl(
-    new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true,
-      },
-      // When active the map will receive updates to the device's location as it changes.
-      trackUserLocation: true,
-      // Draw an arrow next to the location dot to indicate which direction the device is heading.
-      showUserHeading: true,
-    })
-  );
+  const geolocateControl = new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    // Draw an arrow next to the location dot to indicate which direction the device is heading.
+    showUserHeading: true,
+  });
+  map.addControl(geolocateControl);
+
+  geolocateControl.on('geolocate', function (e) {
+    const center = [e.coords.longitude, e.coords.latitude];
+    map.flyTo({
+      center: center,
+      essential: true,
+    });
+  });
+
+  return map;
 }
 
 const firebaseConfig = {
